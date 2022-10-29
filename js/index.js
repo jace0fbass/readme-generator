0fa53ada7d70de1d7282f73fb2b6ce01264ee637
Jason <jajones414@gmail.com>
2022-10-29 14:44:07 -0500

    First commit after moving contents of challenge folder to repo folder and adding questions array and imports to the index.js file.

---
+++ js/index.js
@@ -0,0 +1,41 @@
+// TODO: Include packages needed for this application
+import fs from "fs";
+import inquirer from "inquirer";
+import generateMarkdown from "./generateMarkdown.js";
+console.log(generateMarkdown);
+// TODO: Create an array of questions for user input
+const questions = [
+  { 
+    type: "input", 
+    message: "What was your motivation?", 
+    name: "motivation" },
+  {
+    type: "input",
+    message: "Why did you build this project?",
+    name: "why",
+  },
+  {
+    type: "input",
+    message: "What problem does your project solve?",
+    name: "problemSolved",
+  },
+  {
+    type: "input",
+    message: "What did you learn?",
+    name: "learned",
+  },
+  {
+    type: "input",
+    message: "What makes your project stand out?",
+    name: "standOut",
+  },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
