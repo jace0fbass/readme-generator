4680d014184ff86595beed94d70e888bc3323c27
Jason <jajones414@gmail.com>
2022-10-30 08:40:54 -0500

    I have corrected errors in the inquirer array.

--- js/index.js
+++ js/index.js
@@ -5,35 +5,39 @@ import generateMarkdown from "./generateMarkdown.js";
 console.log(generateMarkdown);
 // TODO: Create an array of questions for user input
 inquirer.prompt([
-  { 
-    type: "input", 
-    message: "What was your motivation?", 
-    name: "motivation", 
-  },
-  {
-    type: "input",
-    message: "Why did you build this project?",
-    name: "why",
-  },
-  {
-    type: "input",
-    message: "What problem does your project solve?",
-    name: "problemSolved",
-  },
-  {
-    type: "input",
-    message: "What did you learn?",
-    name: "learned",
-  },
-  {
-    type: "input",
-    message: "What makes your project stand out?",
-    name: "standOut",
-  },
-]);
+    { 
+      type: "input", 
+      message: "What was your motivation?", 
+      name: "motivation" 
+    },
+    {
+      type: "input",
+      message: "Why did you build this project?",
+      name: "why"
+    },
+    {
+      type: "input",
+      message: "What problem does your project solve?",
+      name: "problemSolved"
+    },
+    {
+      type: "input",
+      message: "What did you learn?",
+      name: "learned"
+    },
+    {
+      type: "input",
+      message: "What makes your project stand out?",
+      name: "standOut"
+    }
+  ])
 
+.then(answers => {
+  const readme = generateMarkdown(answers)
+  console.log(readme)
+})
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
 
 // TODO: Create a function to initialize app
 function init() {}
